7c776f63fc6084004c027c1c97c5ca04464a1766
Yen Colon <yendejesusc41@gmail.com>
2020-12-10 16:39:58 -0400

    wallet module

--- src/views/Wallet.jsx
+++ src/views/Wallet.jsx
@@ -3,10 +3,29 @@ import AddCredit from "../components/AddCredit";
 import CheckCredit from "../components/CheckCredit";
 
 const Wallet = () => {
+  const [showCredit, setShowCredit] = React.useState(true);
   return (
-    <div className='flex flex-row justify-around items-center'>
-      <CheckCredit />
-      <AddCredit />
+    <div className="flex flex-col justify-around items-center pt-10">
+      <div className="bg-gray-400 rounded flex flex-row justify-around">
+        <button
+          onClick={() => setShowCredit(true)}
+          className={`${
+            !showCredit ? "bg-yellow-400" : "bg-yellow-600"
+          } text-center p-3 focus:outline-none active:bg-green-700`}
+        >
+          Consultar
+        </button>
+        <button
+          onClick={() => setShowCredit(false)}
+          className={`${
+            showCredit ? "bg-yellow-400" : "bg-yellow-600"
+          } text-center p-3 focus:outline-none active:bg-green-700`}
+        >
+          Recargar
+        </button>
+      </div>
+      {!showCredit && <AddCredit />}
+      {showCredit && <CheckCredit />}
     </div>
   );
 };
